648e3cf49189149062c26bdbedd5ff8a7d47a95c
Baiwei-Liu-s3778909 <s3778909@student.rmit.edu.au>
2025-03-11 22:47:33 +1100

    change load an image from mosaic.js to client.js

--- js/client.js
+++ js/client.js
@@ -1,1 +1,13 @@
 // Edit me. Feel free to create additional .js files.
+
+
+//load an image from client uploaded image
+async function loadImage(file) {
+    const image = new Image();
+    image.src = URL.createObjectURL(file); //file change to URL to load images
+    await new Promise((resolve, reject) => { //resolve-success, reject-fail
+      image.onload = () => resolve(image); //when success
+      image.onerror = (error) => reject(error); //when fail
+    });
+    return image;
+  }
--- js/mosaic.js
+++ js/mosaic.js
@@ -10,13 +10,3 @@ if (exports) {
   exports.TILE_HEIGHT = TILE_HEIGHT;
 }
 
-//load an image from client uploaded image
-async function loadImage(file) {
-  const image = new Image();
-  image.src = URL.createObjectURL(file); //file change to URL to load images
-  await new Promise((resolve, reject) => { //resolve-success, reject-fail
-    image.onload = () => resolve(image); //when success
-    image.onerror = (error) => reject(error); //when fail
-  });
-  return image;
-}
