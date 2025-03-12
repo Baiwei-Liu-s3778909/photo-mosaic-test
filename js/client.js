33d33d74d0d5acee531cc8355dbb79adff3bd116
Baiwei-Liu-s3778909 <s3778909@student.rmit.edu.au>
2025-03-13 01:52:49 +1100

    add event listener function to load images and process image in mosaic.html

--- js/client.js
+++ js/client.js
@@ -11,3 +11,25 @@ async function loadImage(file) {
     });
     return image;
   }
+  
+//monitor events and excute when the mosaic.html is fully loaded
+document.addEventListener('DOMContentLoaded', function(){
+  const imageInput = document.getElementById('image'); //get id="image" element
+  const mosaicCanvas = document.getElementById('mosaicCanvas'); //get id="mosaicCanvas"
+
+
+  //change is a event and excute when the client choose files
+  imageInput.addEventListener('change', async function(event) {
+    //event.target:file selection box
+    const file = event.target.files[0];
+    if(file){
+      try{
+        const img = await loadImage(file); //excute try function to load an image and process the image
+        processImage(img, mosaicCanvas);
+      } catch(error){ //excute catch if it appears the error 
+        console.error('Failed to load image:', error);
+      }
+    }
+      
+  });
+});
